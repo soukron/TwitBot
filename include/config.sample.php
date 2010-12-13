e2404583a88fccb07ad735a3324340a37b10a7a1
Soukron <soukron@gmbros.net>
2010-12-14 00:20:15 +0100

    Added cleanUpDB action

--- include/config.sample.php
+++ include/config.sample.php
@@ -3,5 +3,5 @@
 	$oauth_consumer_key 	= "<YourConsumerKey>";
 	$oauth_consumer_secret 	= "<YourConsumerSecret>";
 	$db_str 		= "sqlite:/opt/TwitBot/db/bot.db3";
-	$actions		= array("makeFollowers", "getNewDMs", "getNewMentions");
+	$actions		= array("makeFollowers", "getNewDMs", "getNewMentions", "cleanUpDB");
 ?>
